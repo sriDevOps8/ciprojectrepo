95a0f7f3864c250784f6a9070e8e9bce1ec342ab
jarvis <jarvis@HRDC-ENTFRESH>
2025-01-21 23:34:17 +0530

    change again

--- myTEst/src/main/java/web/myTEst/App.java
+++ myTEst/src/main/java/web/myTEst/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!  asfkjlashflkahs" );
+        System.out.println( "Hello World!  change again " );
     }
 }
--- myTEst/src/test/java/stepDefinitions/TestRunner.java
+++ myTEst/src/test/java/stepDefinitions/TestRunner.java
@@ -1,12 +1,48 @@
 package stepDefinitions;
 
+import org.testng.annotations.DataProvider;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.FeatureWrapper;
+import io.cucumber.testng.PickleWrapper;
 
-@CucumberOptions(
-    features = "src/test/resources/features",
-    glue = "stepDefinitions",
-    plugin = {"pretty", "html:target/cucumber-reports.html"}
-)
-public class TestRunner extends AbstractTestNGCucumberTests {
-}
+import org.testng.annotations.DataProvider;
+import java.util.concurrent.CountDownLatch;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+		@CucumberOptions(
+				  features = "src/test/resources/features",
+				  glue = {"stepDefinitions"},
+				  plugin = {"pretty", "json:target/cucumber.json"},
+				  tags = "@smoke"
+				)
+		public class TestRunner extends AbstractTestNGCucumberTests {
+
+		    private static final int MAX_THREADS = 1;  // Control number of concurrent threads
+
+		    @Override
+		    @DataProvider(parallel = false)  // Set parallel to false
+		    public Object[][] scenarios() {
+		        return super.scenarios();
+		    }
+
+		    @Override
+		    public void runScenario(PickleWrapper pickleWrapper, FeatureWrapper featureWrapper) {
+		        // Create a thread pool with a fixed number of threads
+		        ExecutorService executorService = Executors.newFixedThreadPool(MAX_THREADS);
+		        
+		        executorService.submit(() -> {
+		            // Run the scenario
+		            super.runScenario(pickleWrapper, featureWrapper);
+		        });
+
+		        executorService.shutdown();
+		    }
+		}
